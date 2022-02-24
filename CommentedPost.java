2cb112b5ad1d2b215e8f6acf8e6c5bdc7112af55
lab596 <86579136+lab596@users.noreply.github.com>
2022-02-23 21:19:52 -0800

    Create CommentedPost.java

---
+++ CommentedPost.java
@@ -0,0 +1,69 @@
+
+import java.util.ArrayList;
+/**
+ * Write a description of class CommentedPost here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CommentedPost extends Post
+{
+    int likes;
+    ArrayList<String> comments = new ArrayList<String>();
+    
+    public CommentedPost(String author)
+    {
+        super(author);
+        timestamp = System.currentTimeMillis();
+        likes = 0;
+        comments = new ArrayList<>();
+    }
+
+    /**
+     * Record one more 'Like' indication from a user.
+     */
+    public void like()
+    {
+        likes++;
+    }
+
+    /**
+     * Record that a user has withdrawn his/her 'Like' vote.
+     */
+    public void unlike()
+    {
+        if (likes > 0) {
+            likes--;
+        }
+    }
+
+    /**
+     * Add a comment to this post.
+     * 
+     * @param text  The new comment to add.
+     */
+    public void addComment(String text)
+    {
+        comments.add(text);
+    }
+    public void display()
+    {
+        super.display();
+        if(likes > 0) {
+            System.out.println("  -  " + likes + " people like this.");
+        }
+        else {
+            System.out.println();
+        }
+        
+        if(comments.isEmpty()) {
+            System.out.println("   No comments.");
+        }
+        else {
+            System.out.println("   " + comments.size() + " comment(s). Click here to view.");
+        }
+    }
+    
+}
+
+
